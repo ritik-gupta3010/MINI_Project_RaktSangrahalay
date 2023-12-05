3702b58ce5007cf1800a076d18ff0c4296333657
ritik-gupta3010 <81680663+ritik-gupta3010@users.noreply.github.com>
2023-12-05 16:59:08 +0530

    Update fireConfig.js

--- Script/fireConfig.js
+++ Script/fireConfig.js
@@ -1,13 +1,23 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyBCx_Tl5K0OXmm3m6Vx-OFjj_n3uPIfQO8",
-  authDomain: "mini-project-d5b7b.firebaseapp.com",
-  databaseURL: "https://mini-project-d5b7b-default-rtdb.firebaseio.com",
-  projectId: "mini-project-d5b7b",
-  storageBucket: "mini-project-d5b7b.appspot.com",
-  messagingSenderId: "958969821130",
-  appId: "1:958969821130:web:5116b316750b2840200133",
-  measurementId: "G-WDLV7XJNE9"
-  };
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBCx_Tl5K0OXmm3m6Vx-OFjj_n3uPIfQO8",
+//   authDomain: "mini-project-d5b7b.firebaseapp.com",
+//   databaseURL: "https://mini-project-d5b7b-default-rtdb.firebaseio.com",
+//   projectId: "mini-project-d5b7b",
+//   storageBucket: "mini-project-d5b7b.appspot.com",
+//   messagingSenderId: "958969821130",
+//   appId: "1:958969821130:web:5116b316750b2840200133",
+//   measurementId: "G-WDLV7XJNE9"
+//   };
+const firebaseConfig = {
+  apiKey: "AIzaSyDClZ1JvEoySKT64P9o7QEDOu4178j7tiU",
+  authDomain: "raktsangrahalay.firebaseapp.com",
+  databaseURL: "https://raktsangrahalay-default-rtdb.firebaseio.com",
+  projectId: "raktsangrahalay",
+  storageBucket: "raktsangrahalay.appspot.com",
+  messagingSenderId: "292617549545",
+  appId: "1:292617549545:web:a94ad89e6606cd04cce278",
+  measurementId: "G-CHDZ6E2XCB"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   const auth =  firebase.auth();
